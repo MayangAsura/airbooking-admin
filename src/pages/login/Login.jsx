797b66e943f619363822b3b63d8da29bf6ac61eb
MayangAsura <39020906+MayangAsura@users.noreply.github.com>
2025-12-17 18:31:38 +0700

    updated login base api url
    updated

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,6 +4,8 @@ import { useNavigate} from 'react-router-dom'
 import { AuthContext } from "../../context/AuthContext";
 import axios from "axios";
 
+const BASE_URL = process.env.REACT_APP_SERVER_MODE === 'production'? PROD_URL : LOCAL_URL
+
 const Login = () => {
   const [credentials, setCredentials] = useState({
     username: undefined,
@@ -25,7 +27,8 @@ const Login = () => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post("/auth/login", credentials);
+      const res = await axios.post( `${BASE_URL}/auth/login`, credentials);
+      // const res = await axios.post("/auth/login", credentials);
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
       navigate('/')
     } catch (error) {
